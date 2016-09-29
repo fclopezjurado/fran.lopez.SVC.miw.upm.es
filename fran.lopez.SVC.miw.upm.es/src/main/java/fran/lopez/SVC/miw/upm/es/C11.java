6c3f413d9c1d89fda7e03447b837d6868094372f
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-09-29 22:24:27 +0200

    Create C11 & m1() y m2()

---
+++ fran.lopez.SVC.miw.upm.es/src/main/java/fran/lopez/SVC/miw/upm/es/C11.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fran.lopez.SVC.miw.upm.es;
+
+/**
+ * @author franlopez
+ *
+ */
+public class C11 {
+	
+	public String m1() {
+		return "m1";
+	}
+	
+	public String m2() {
+		return "m2";
+	}
+
+}
