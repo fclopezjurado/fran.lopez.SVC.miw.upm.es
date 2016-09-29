2efa2c97cf8c8dde3dc79ae22926109e6f09a4cb
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-09-30 00:05:04 +0200

    Add m3() method to C21 class and add a test for m3() method to C21Test

--- fran.lopez.SVC.miw.upm.es/src/main/java/fran/lopez/SVC/miw/upm/es/C21.java
+++ fran.lopez.SVC.miw.upm.es/src/main/java/fran/lopez/SVC/miw/upm/es/C21.java
@@ -18,5 +18,9 @@ public class C21 {
 	public String m2() {
 		return "m2";
 	}
+	
+	public String m3() {
+		return "m3";
+	}
 
 }
--- fran.lopez.SVC.miw.upm.es/src/test/java/fran/lopez/SVC/miw/upm/es/C21Test.java
+++ fran.lopez.SVC.miw.upm.es/src/test/java/fran/lopez/SVC/miw/upm/es/C21Test.java
@@ -34,5 +34,13 @@ public class C21Test {
 	public void testM2() {
 		assertEquals("m2", this.objectFromC21Class.m2());
 	}
+	
+	/**
+	 * Test method for {@link fran.lopez.SVC.miw.upm.es.C21#m2()}.
+	 */
+	@Test
+	public void testM3() {
+		assertEquals("m3", this.objectFromC21Class.m3());
+	}
 
 }
