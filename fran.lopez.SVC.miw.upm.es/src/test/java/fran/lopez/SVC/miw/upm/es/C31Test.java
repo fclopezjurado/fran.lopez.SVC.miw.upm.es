45dac17619eaea9bb14707868446d3e5b13b3d59
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-09-29 23:22:17 +0200

    Create C31Test class

---
+++ fran.lopez.SVC.miw.upm.es/src/test/java/fran/lopez/SVC/miw/upm/es/C31Test.java
@@ -0,0 +1,26 @@
+package fran.lopez.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C31Test {
+	private C31 objectFromC31Class;
+
+	@Before
+    public void before() {
+        this.objectFromC31Class = new C31();
+    }
+
+	@Test
+	public void testM1() {
+		assertEquals("m1", this.objectFromC31Class.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", this.objectFromC31Class.m2());
+	}
+
+}
